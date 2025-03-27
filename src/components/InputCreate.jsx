312e0ece6de69c00df111e1187e00b9f0e9ab0d1
MarianaLGM <mariana_lobeto@hotmail.com>
2025-03-27 13:36:21 +0100

    cambios

---
+++ src/components/InputCreate.jsx
@@ -0,0 +1,3 @@
+//donde crearás un `input` y un `botón`
+//La funcionalidad será que al insertar la tarea en el input y darle al botón enviar se añadirá a nuestra BBDD y
+//  por tanto desde la ruta `"/"` podremos ver todos las tareas anteriores junto con las que añadamos desde React
